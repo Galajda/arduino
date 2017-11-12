b0b768fab6f1762f2c89b8d1156aba096977e87b
Galajda <33378775+Galajda@users.noreply.github.com>
2017-11-12 09:53:38 -0600

    simplified motor control example
    To be used with Velleman KA-03 Arduino shield.

---
+++ simplified_velleman_ka03_example.ino
@@ -0,0 +1,79 @@
+//copied from Vellman example
+int pwm_a = 3;  //PWM control for motor outputs 1 and 2 
+int dir_a = 2;  //direction control for motor outputs 1 and 2 
+
+void setup()
+
+{
+  
+pinMode(pwm_a, OUTPUT);  //Set control pins to be outputs
+  
+pinMode(dir_a, OUTPUT);
+  
+}
+
+
+
+void loop()
+{
+  
+
+digitalWrite(dir_a, LOW); 
+  
+  
+  
+analogWrite(pwm_a, 50);  
+  
+delay(1000);
+  
+  
+
+analogWrite(pwm_a, 100);  
+  
+delay(1000);
+  
+ 
+ 
+analogWrite(pwm_a, 50);  
+
+delay(1000);
+  
+ 
+ 
+analogWrite(pwm_a, 0);  
+
+delay(1000);
+  
+  
+
+digitalWrite(dir_a, HIGH); 
+  
+
+analogWrite(pwm_a, 50);  
+  
+delay(1000);
+  
+  
+
+analogWrite(pwm_a, 100);  
+  
+delay(1000);
+  
+ 
+ 
+analogWrite(pwm_a, 50);  
+
+delay(1000);
+  
+ 
+ 
+analogWrite(pwm_a, 0);  
+
+delay(1000);
+  
+  
+
+
+}
+
+
